b9252927b1590a4ea18af5c0fb17cc421207857e
ZCDEV <zcdev@Orange.attlocal.net>
2019-01-24 21:07:21 -0800

    Updates

---
+++ precache-manifest.77c255249c773021bd793a7cc482a6fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f871c7807c4ca344a2a",
+    "url": "/clicky-owls/static/js/runtime~main.7f871c78.js"
+  },
+  {
+    "revision": "ae2b73df809a480c051d",
+    "url": "/clicky-owls/static/js/main.ae2b73df.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky-owls/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "ae2b73df809a480c051d",
+    "url": "/clicky-owls/static/css/main.188f1621.chunk.css"
+  },
+  {
+    "revision": "679e8b97edb3fd07f1bc773148f6e1b7",
+    "url": "/clicky-owls/index.html"
+  }
+];
